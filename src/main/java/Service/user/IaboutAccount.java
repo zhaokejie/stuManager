582268439265adf8db7b5ed7627e93228a5e82fb
zhaokejie <870262949@qq.com>
2020-11-23 22:51:28 +0800

    尝试版本回退!!!!!!

--- src/main/java/Service/user/IaboutAccount.java
+++ src/main/java/Service/user/IaboutAccount.java
@@ -10,3 +10,10 @@ public interface IaboutAccount {
     public UserType getUserType(int UID);
     //返回用户的类型学生 or 管理员
 }
+
+
+//调用DAO层获取用户名是否存在
+// 若存在取得该用户名的密码
+//判断密码是否正确
+//正确就返回登陆成功，错误就返回登陆失败
+//若登陆成功再从DAO层获取用户其他需要的临时数据存入Session管理器（内存中）
